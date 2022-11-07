a7a6c94761df5e4d81073f7b69cda1867f4b7de3
Jani Jegoroff <jani.jegoroff@gmail.com>
2022-11-07 17:13:20 +0000

    feat: truffle

---
+++ truffle/test/spacebear-test.js
@@ -0,0 +1,14 @@
+const Spacebear = artifacts.require("Spacebear");
+const truffleAssert = require('truffle-assertions');
+
+contract("Spacebear", (accounts) => {
+  it('should credit NFT to a correct account', async () => {
+    const spacebearInstance = await Spacebear.deployed();
+    const txResult = await spacebearInstance.safeMint(accounts[1], "spacebear_1.json");
+    truffleAssert.eventEmitted(txResult, 'Transfer', {from: '0x0000000000000000000000000000000000000000',
+                                                      to: accounts[1],
+                                                      tokenId: web3.utils.toBN("0")});
+    const owner = await spacebearInstance.ownerOf(0);
+    assert.equal(owner, accounts[1]);
+  });
+});
