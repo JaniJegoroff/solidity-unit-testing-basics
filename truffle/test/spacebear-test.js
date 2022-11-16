04618dffca90272cdedb9077620c2d0e4c3f5470
Jani Jegoroff <jani.jegoroff@gmail.com>
2022-11-16 21:14:28 +0000

    refactor(truffle): cleanup and token uri assertion

--- truffle/test/spacebear-test.js
+++ truffle/test/spacebear-test.js
@@ -8,16 +8,21 @@ contract('Spacebear', (accounts) => {
     spacebearInstance = await Spacebear.deployed();
   });
 
-  it('should return correct contract name', async () => {
+  it('should return contract name', async () => {
     assert.equal(await spacebearInstance.name(), 'Spacebear');
   });
 
-  it('should credit NFT to a correct account', async () => {
+  it('should credit token to a correct account', async () => {
     const txResult = await spacebearInstance.safeMint(accounts[1], 'spacebear_1.json');
     truffleAssert.eventEmitted(txResult, 'Transfer', {from: '0x0000000000000000000000000000000000000000',
                                                       to: accounts[1],
                                                       tokenId: web3.utils.toBN('0')});
+    
     const owner = await spacebearInstance.ownerOf(0);
     assert.equal(owner, accounts[1]);
+
+    const expectedTokenURI = 'https://ethereum-blockchain-developer.com/2022-06-nft-truffle-hardhat-foundry/nftdata/spacebear_1.json';
+    const tokenURI = await spacebearInstance.tokenURI(0);
+    assert.equal(tokenURI, expectedTokenURI);
   });
 });
